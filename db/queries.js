c93b747ea3e24246072c6ea36f9facb930e448a4
Carl Benbow <cbenbow@synergisticagility.com>
2021-05-25 23:51:02 -0400

    cleaned up formatting

--- db/queries.js
+++ db/queries.js
@@ -1,78 +1,72 @@
 const connection = require("./connection");
 
-
-
-
 class DB {
   constructor(connection) {
-     this.connection = connection;
-  } 
+    this.connection = connection;
+  }
   findAllEmployees() {
     return this.connection.query(
-      "SELECT employee.id, employee.first_name,employee.last_name, role.title, department.name AS department, role.salary,CONCAT(manager.first_name,  '  ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;",
+      "SELECT employee.id, employee.first_name,employee.last_name, role.title, department.name AS department, role.salary,CONCAT(manager.first_name,  '  ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
     );
   }
 
-  findAllEmployeesWithoutManagers(){
-    return this.connection.query(`SELECT employee.id, employee.first_name,employee.last_name, role.title, department.name AS department, role.salary,CONCAT(manager.first_name,  '  ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id
-    where employee.manager_id is not NULL`)
+  findAllEmployeesWithoutManagers() {
+    return this.connection
+      .query(`SELECT employee.id, employee.first_name,employee.last_name, role.title, department.name AS department, role.salary,CONCAT(manager.first_name,  '  ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id
+    where employee.manager_id is not NULL`);
   }
 
   findAllEmployeesByDepartment = (dept) => {
-    return this.connection.query(` SELECT employee.id, employee.first_name,employee.last_name, role.title, department.name AS department, role.salary,CONCAT(manager.first_name, ' ' , manager.last_name) AS manager 
+    return this.connection
+      .query(` SELECT employee.id, employee.first_name,employee.last_name, role.title, department.name AS department, role.salary,CONCAT(manager.first_name, ' ' , manager.last_name) AS manager 
     FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id
   WHERE department.id = ${dept}`);
- 
-}
+  };
 
-findAllEmployeesByManager = (manager) => {
-  return this.connection.query(` SELECT employee.id, employee.first_name,employee.last_name, role.title, department.name AS department, role.salary,CONCAT(manager.first_name, ' ' , manager.last_name) AS manager 
+  findAllEmployeesByManager = (manager) => {
+    return this.connection
+      .query(` SELECT employee.id, employee.first_name,employee.last_name, role.title, department.name AS department, role.salary,CONCAT(manager.first_name, ' ' , manager.last_name) AS manager 
   FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id
 WHERE employee.manager_id = ${manager}`);
-
-} 
-  getManagersList(){
+  };
+  getManagersList() {
     return this.connection.query(
-    `SELECT employee.id, employee.first_name,employee.last_name, role.title, department.name AS department, role.salary,CONCAT(manager.first_name, ' ' , manager.last_name) AS manager 
+      `SELECT employee.id, employee.first_name,employee.last_name, role.title, department.name AS department, role.salary,CONCAT(manager.first_name, ' ' , manager.last_name) AS manager 
     FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id
-  WHERE employee.manager_id is NULL` );
+  WHERE employee.manager_id is NULL`
+    );
   }
 
-  getRoles(){
-    return this.connection.query(
-    `SELECT * FROM employee_managementdb.role` );
+  getRoles() {
+    return this.connection.query(`SELECT * FROM employee_managementdb.role`);
   }
-  getDepartments(){
+  getDepartments() {
     return this.connection.query(
-    `SELECT * FROM employee_managementdb.department` );
+      `SELECT * FROM employee_managementdb.department`
+    );
   }
-  //get department query here
 
-  addEmployee =(firstName, lastName,role, managerId) => {
-    return this.connection.query(`INSERT INTO employee (first_name,last_name, role_id,manager_id) VALUES ('${firstName}', '${lastName}',${role},${managerId})`);
-    
-    
-  }
+
+  addEmployee = (firstName, lastName, role, managerId) => {
+    return this.connection.query(
+      `INSERT INTO employee (first_name,last_name, role_id,manager_id) VALUES ('${firstName}', '${lastName}',${role},${managerId})`
+    );
+  };
   deleteEmployee = (id) => {
-  
-    return this.connection.query(`DELETE FROM employee where id = ${id}`)
-  }
+    return this.connection.query(`DELETE FROM employee where id = ${id}`);
+  };
 
-  updateEmployeeRole =(role,id) => {
-  
-    return this.connection.query(`UPDATE employee SET role_id =${role} where id =${id}`) 
-    
-  }
+  updateEmployeeRole = (role, id) => {
+    return this.connection.query(
+      `UPDATE employee SET role_id =${role} where id =${id}`
+    );
+  };
 
-
-  updateEmployeeManager =(manager, id) => {
-  
-    return this.connection.query(`UPDATE employee SET manager_id =${manager} where id =${id}`) 
-    
-  }
-  
-  
+  updateEmployeeManager = (manager, id) => {
+    return this.connection.query(
+      `UPDATE employee SET manager_id =${manager} where id =${id}`
+    );
+  };
 }
 
 module.exports = new DB(connection);
-
